9349af85abda6e4d1de791b6addab811554013a1
GeoArchonMorax <korisawarezero@gmail.com>
2024-11-13 10:26:38 +0800

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -1,18 +1,10 @@
 const { exec } = require('child_process');
 
-// Replace this with your actual admin access token
-const adminAccessToken = 'EAAU7g4CKVBkBOyLMhMKmRPv3l7JQS1tUZA30gcaVJIoatZCHXqaOOVr9N6kpstZClDZASlD8yHZCpjWKGH1H8IcAWZBKv1kRxZBZAgp6ZAh4ZA5qxXwvc8lZCRvxmi4Tv9kEF7ZCL8x5yVaVesS7Ay3sJxSXPDlmA9TWQTQVEx44wNYBa5aZBouMSJXVOls07Tw4OiydhdXpg4h93M0sg1uS8PDIAjELZBIyR7'; // Store your actual access token here
-
 module.exports = {
   name: 'update',
-  description: 'Updates bot dependencies.',
-  author: 'Aljur Pogoy',
+  description: 'Updates bot dependencies.', // Note: No restart mentioned here
+  author: 'Your Name',
   execute(senderId, args, pageAccessToken, sendMessage) {
-    if (pageAccessToken !== adminAccessToken) {
-      sendMessage(senderId, { text: 'You do not have permission to update dependencies.' }, pageAccessToken);
-      return;
-    }
-
     sendMessage(senderId, { text: 'Updating dependencies...' }, pageAccessToken);
     exec('npm install', (error, stdout, stderr) => {
       if (error) {
