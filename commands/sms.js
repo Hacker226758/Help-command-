820f9f9d4dd0b7795bf4fda6fe497c046eeaef6d
Developer GilgameshV2 <127070765+Hacker226758@users.noreply.github.com>
2024-11-25 16:02:03 +0800

    Update sms.js

--- commands/sms.js
+++ commands/sms.js
@@ -1,36 +1,30 @@
 const axios = require('axios');
-const { sendMessage } = require('../handles/sendMessage');
-
 module.exports = {
   name: 'sms',
-  description: 'Send Free Text',
-  usage: 'sms phone# | yourmessage',
-  admin: true,
+  description: 'Send SMS using API.',
+  author: 'Aljur Pogoy',
+  async execute(senderId, args, pageAccessToken, sendMessage) {
+    const phoneNumber = args[0];
+    const message = args.slice(1).join(' ');
 
-  async execute(senderId, args, pageAccessToken,) {
-
-    const text = args.join(" ");
-
-    const text1 = text.substr(0, text.indexOf(' | ')).trim();
-    const text2 = text.split(' | ').pop().trim();
-
-    if (!text1 || !text2) {
-      return sendMessage(senderid, {
-        text: formatresponse('Please provide both a question and an answer. Example: 09123456789 | yourmessage')
-      }, pageaccesstoken);
+    if (!phoneNumber || !message) {
+      sendMessage(senderId, { text: 'Invalid format. Use: sms [phone number] [message]' }, pageAccessToken);
+      return;
     }
 
+    const apiUrl = `https://api.kenliejugarap.com/freesmslbc/?number=${encodeURIComponent(phoneNumber)}&message=${encodeURIComponent(message)}`;
+    
     try {
-       const response = await axios.get(`https://api.kenliejugarap.com/freesmslbc/?number=${encodeURIComponent(text1)}&message=${encodeURIComponent(text2)}`);
-       
-    const respo = response.data.response;
-      const simnet = response.data.sim_network;
-      
-      const message = `Message: ${respo}\nSending Text: ${text2}\nSend To: ${text1}\nNetwork: ${simnet}\n
-`;
-      await sendMessage(senderId, message, pageAccessToken);
-    } catch {
-      sendMessage(senderId, { text: 'There was an error generating the content. Please try again later.' }, pageAccessToken);
+      const response = await fetch(apiUrl);
+      const data = await response.json();
+
+      if (data.status) {
+        sendMessage(senderId, { text: 'SMS sent successfully!' }, pageAccessToken);
+      } else {
+        sendMessage(senderId, { text: data.response }, pageAccessToken);
+      }
+    } catch (error) {
+      sendMessage(senderId, { text: 'Error sending SMS. Please try again later.' }, pageAccessToken);
     }
   }
 };
